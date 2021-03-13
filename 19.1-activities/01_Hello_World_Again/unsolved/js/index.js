8572ef41beab1783859c1b2584fb873cf0addfe8
hklarich <71339042+hklarich@users.noreply.github.com>
2021-03-13 14:09:51 -0500

    19.1 activities

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,13 @@
 // Create a variable below
 
+var helloWorld='Heather here';
+
 // Console.log() your variable here.
+
+console.log(helloWorld);
+
+/*alert(helloWorld);
+
+alert("Yes, I made it!");*/
+
+
--- 19.1-activities/02_More_Variables/unsolved/js/index.js
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -1,3 +1,15 @@
 // Create the addNumbers function here.
 
+var peopleup = 2;
+var peopledown = 3;
+var z = 'There are ' + (peopleup+peopledown) + ' people all together';
+
 // Create the stringConcat function here.
+
+function addNumbers() {
+    console.log(z);
+     
+}
+
+addNumbers();
+
