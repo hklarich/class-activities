85ccb3be465f2f085ff26edc0da7c08628857bd9
hklarich <71339042+hklarich@users.noreply.github.com>
2021-03-17 18:41:58 -0400

    update

---
+++ 19.2-activities/02_theme_changer/unsolved/js/themeChange.js
@@ -0,0 +1,14 @@
+$(".themeChange").on("click", function(){
+    if($(this).hasClass("regular")){
+        $("#theme").attr("href","css/blank.css")
+        console.log("it works!");
+
+    }
+    else if ($(this).hasClass("blue")){
+        $("#theme").attr("href","css/blue.css")
+        console.log("it's blue!");
+    }
+    else if ($(this).hasClass("dark")){
+        $("#theme").attr("href","css/dark.css")
+        console.log("it's dark!");
+    }})
